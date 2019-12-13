08fd7847b74e1ddb739f27199b806a41fbac5c77
Anton <asavienko@gmail.com>
2019-12-13 14:23:22 +0200

    deleted mockData

--- api/toDellete/constants/constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  searchRequest:
-    "https://rabota.ua/employer/find/cv_list?keywords=%D0%9C%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80+%D0%BF%D0%BE+%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B0%D0%BC%2C&period=7&searchtype=everywhere&regionid=21&moveability=0&sort=date",
-  browserHeadless: "headless",
-};
