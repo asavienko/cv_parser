d7835279bf478022b24d8a192e845c30daf81c51
Anton <asavienko@gmail.com>
2020-01-05 16:21:50 +0200

    applying server validation

---
+++ api/validationSchemas/signUpValidation.js
@@ -0,0 +1,17 @@
+const Joi = require("@hapi/joi");
+const { EMAIL_VALIDATION } = require("../constants/validation");
+
+const schema = Joi.object({
+  email: Joi.string()
+    .required()
+    .max(90)
+    .email(),
+  password: Joi.string().required().max(90).valid(EMAIL_VALIDATION)
+  /*
+  confirm: delete from UI
+
+  phone: only "+" and numbers
+  name:
+  surname:*/
+
+});
